fbdff0dc34ee3c3feb12393ab1cb473e52a42d7c
Boris Tam <Boris Tam>
2017-01-21 18:50:36 -0800

    for loop fix

--- main/parsejson.py
+++ main/parsejson.py
@@ -1,23 +1,27 @@
-import json
+mport json
 import glob, os
+from os.path import join, dirname
 from pprint import pprint
 
 # object that keeps track of category and count
-def ClassCounter:
-	
-	def __init__(self, name):
-		self.name = name
-		self.count = 0
+class ClassCounter(object):
+    def __init__(self, name):
+        self.name = name
+        self.count = 0
 
-	def inc():
-		self.count = self.count + 1
+    def inc(self):
+        self.count += 1
 
 # updates the list elements
 def update_class(obj, listobj):
-	if (obj in listobj):
-		list[obj].inc()
-	else:
-		list.append(obj)
+    #if len(listobj) == 0:
+    listobj.append(obj)
+    #for x in listobj:
+        #if (x.name == obj.name):
+    x.count += 1
+
+    # else:
+    #     listobj.append(obj)
 
 
 # change the directory to access source images
@@ -29,26 +33,30 @@ classifiers_id = "ManvsWoman"
 
 # parsing the json files
 for file in glob.glob("*.json"):
-	with open(file) as data_file:    
-    	data = json.load(data_file)
+    with open(join(dirname(__file__), file), 'r') as data_file:    
+        data = json.load(data_file)
+# only want classification from custom classifier
+        accessor = data['images'][0]['classifiers'][0]
+        if( classifiers_id in accessor['name']):
+            myList = []
+            myObj = ClassCounter(accessor['classes'][0]['class'])
+            update_class(myObj, myList)
 
-		# only want classification from custom classifier
-		accessor = data['images'][0]['classifiers'][0]
-		if( classifiers_id in accessor['name']):
-			pprint(accessor['classes'][0]['class'])
-			pprint(accessor['classes'][0]['score'])
+for p in myList:
+    print(p.name)
+    print(p.count)
 
-arraylist of objects
-object counter = {
-	name = 'male',
-	count = 0
-}
+# arraylist of objects
+# object counter = {
+#     name = 'male',
+#     count = 0
+# }
 
-# for every male found 
-# 	count increments
+# for every male found
+#     count increments
 
-# in the arraylist, many categories, for example (male, female, cat, orange) 
-# 	sort the arraylist with comparator of the count
+# in the arraylist, many categories, for example (male, female, cat, orange)
+#     sort the arraylist with comparator of the count
 
 # select the top 5 categories
 
